e1c16f78b2c3b83d4c20951c3151da500e04935a
Vanamali Sedimbi <128423291+vanamalisedimbi@users.noreply.github.com>
2023-08-20 23:43:07 +0530

    Add files via upload

--- rec.py
+++ rec.py
@@ -2,18 +2,15 @@ import numpy as np
 import pandas as pd
 from sklearn.metrics.pairwise import cosine_similarity
 
-# Read dataset from text file
 def read_dataset(file_path):
     df = pd.read_csv(file_path)
     return df
 
-# Calculate user similarity using cosine similarity
 def calculate_user_similarity(user_profiles):
     user_similarity = cosine_similarity(user_profiles)
     user_similarity_df = pd.DataFrame(user_similarity, columns=user_profiles.index, index=user_profiles.index)
     return user_similarity_df
 
-# Recommend products based on similar users
 def recommend_products(active_user, user_similarity_df, interactions, num_recommendations=3):
     similar_users = user_similarity_df[active_user].sort_values(ascending=False)[1:num_recommendations+1].index
     recommended_products = {}
@@ -30,24 +27,37 @@ def recommend_products(active_user, user_similarity_df, interactions, num_recomm
     sorted_recommendations = sorted(recommended_products.items(), key=lambda x: x[1], reverse=True)
     return [product for product, _ in sorted_recommendations]
 
-# Main function
 def main():
-    file_path = 'example_data.txt'
-    df = read_dataset(file_path)
+    training_file_path = input("Enter the path of the training dataset file: ")
+    training_df = read_dataset(training_file_path)
 
-    user_profiles = pd.get_dummies(df['Product']).groupby(df['User']).mean()
+    user_profiles = pd.get_dummies(training_df['Product']).groupby(training_df['User']).mean()
     user_similarity_df = calculate_user_similarity(user_profiles)
 
-    interactions = df[['User', 'Product']]
-    unique_users = df['User'].unique()
+    interactions = training_df[['User', 'Product']]
+    unique_users = training_df['User'].unique()
 
     personalized_rankings = {}
     for user in unique_users:
         recommended_products_list = recommend_products(user, user_similarity_df, interactions)
         personalized_rankings[user] = recommended_products_list
 
-    for user, rankings in personalized_rankings.items():
-        print(f"{user}'s Personalized Rankings: {rankings}")
+    test_file_path = input("Enter the path of the test dataset file: ")
+    test_df = read_dataset(test_file_path)
 
-if _name_ == "_main_":
+    accuracy_scores = []
+    for user in unique_users:
+        if user in test_df['User'].unique():
+            actual_interactions = test_df[test_df['User'] == user]['Product'].tolist()
+            recommended_interactions = personalized_rankings[user]
+
+            correct_recommendations = set(actual_interactions) & set(recommended_interactions)
+            accuracy = len(correct_recommendations) / len(actual_interactions) * 100
+            accuracy_scores.append(accuracy)
+            print(f"Accuracy for {user}: {accuracy:.2f}%")
+
+    overall_accuracy = sum(accuracy_scores) / len(accuracy_scores)
+    print(f"Overall Accuracy: {overall_accuracy:.2f}%")
+
+if __name__ == "__main__":
     main()
